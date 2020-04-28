dfca514a7e0e42f01cf7ba5a6e98779156a2f39f
bh-fst02 <bh.fst02@gmail.com>
2020-04-28 14:52:12 +0200

    display mario

--- js/srcipt.js
+++ js/srcipt.js
@@ -4,16 +4,19 @@ let mapHeight = 959;
 let mapWidth = 1024;
 let groundPositionY = 839;
 let ctx;
+let marioPositionY = 539;
+let marioHeight = 300;
+let marioWidth = 300;
 
 const ground = {
   draw() {
     ctx = document.getElementById('ground').getContext('2d');
     let img = new Image();
     img.src = 'images/ground-small.png';
-    img.onload = function() {
+    img.onload = function () {
       for (let i = 0; i * groundWidth < mapWidth; i++) {
-        ctx.drawImage(img, i * groundWidth, groundPositionY, groundWidth, groundHeight); 
-      }   
+        ctx.drawImage(img, i * groundWidth, groundPositionY, groundWidth, groundHeight);
+      }
     }
   },
 }
@@ -23,10 +26,22 @@ const map = {
     ctx = document.getElementById('frame').getContext('2d');
     let img = new Image();
     img.src = 'images/bg.jpg';
-    img.onload = function() {
-    ctx.drawImage(img, 0, 0);
-    ground.draw();
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      ground.draw();
     }
   },
 }
-map.draw();
+
+const mario = {
+  draw() {
+    ctx = document.getElementById('mario').getContext('2d');
+    let img = new Image();
+    img.src = 'images/ogMario.png';
+    img.onload = function () {
+      ctx.drawImage(img, 0, marioPositionY, marioWidth, marioHeight);
+    }
+    map.draw();
+  },
+}
+mario.draw();
