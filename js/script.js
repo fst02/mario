e2269173034f1c9a5d04369cf2128c66e2596ca5
bh-fst02 <bh.fst02@gmail.com>
2020-04-30 16:53:28 +0200

    #FST2-19WIP create pipes and Mario stops before  pipes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,29 @@ let leftPressed;
 let upPressed;
 let initX = 0;
 let initY = 400;
+let groundLevel;
+let marioBack = document.getElementById('mario').offsetLeft;
+let marioFront = marioBack + 88;
+let pipeOffset = 400;
+
+function calcPipeBack() {
+  let gameFieldX = Math.abs(document.getElementById('gameField').offsetLeft); 
+  let pipeBack = (pipeOffset - (gameFieldX % pipeOffset));
+  return pipeBack;
+}
+
+function createPipes(quantity) {
+  let pipe1 = document.createElement('div');
+  pipe1.setAttribute('id', 'pipe');
+  document.getElementById('gameField').appendChild(pipe1);
+  let pipe = document.getElementById('pipe');
+  for (let i = 0; i < quantity; i++) {
+    let pipeClone = pipe.cloneNode(true);
+    pipeClone.style.left = `${i * pipeOffset}px`;
+    document.getElementById('gameField').appendChild(pipeClone);
+  }
+}
+createPipes(10);
 
 function eventListener() {
   document.addEventListener('keydown', keyDownHandler, false);
@@ -19,11 +42,28 @@ function marioDown() {
   }, 50);
 }
 
+function onGround() {
+  if (document.getElementById('mario').offsetTop === 400) {
+    groundLevel = true;
+    return groundLevel;
+  }
+}
+
+function marioBeforePipe() {
+  if (marioFront + 2 === calcPipeBack()) {
+    rightPressed = false;
+  } 
+
+}
+
 function keyDownHandler(event) {
   if (event.keyCode === 39) {
     rightPressed = true;
+    marioBeforePipe();
+    if (rightPressed) {    
     initX -= 10;
-    document.getElementById('gameField').style.left = `${initX}px`;
+    document.getElementById('gameField').style.left = `${initX}px`;       
+     }  
   }
   if (event.keyCode === 37 && initX < 330) {
     leftPressed = true;
@@ -31,9 +71,19 @@ function keyDownHandler(event) {
     document.getElementById('gameField').style.left = `${initX}px`;
   }
   if (event.keyCode === 38 && initY > 200) {
-    upPressed = true;
-    initY -= 100;
-    document.getElementById('mario').style.top = `${initY}px`;
+    let isOnGround = onGround();
+    if (isOnGround) {
+      upPressed = true;
+      initY -= 100;
+      document.getElementById('mario').style.top = `${initY}px`;
+      marioDown();
+    }
+    if (upPressed) {
+      setTimeout(() => {
+        initY -= 100;
+        document.getElementById('mario').style.top = `${initY}px`;
+      }, 100);
+    }
   }
 }
 
@@ -46,16 +96,7 @@ function keyUpHandler(event) {
   }
   if (event.keyCode === 38) {
     upPressed = false;
-    marioDown();
   }
 }
-
-// TODO
-// function createPipes(quantity) {
-//  let pipe = document.getElementById('pipe');
-//  for (let i = 0; i < quantity; i++) {
-//    document.getElementById('gameField').appendChild(pipe);
-//  }
-// }
 
 eventListener();
