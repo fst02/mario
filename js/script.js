473c00b46a9b0f804dad085f1364c1a691205283
bh-fst02 <bh.fst02@gmail.com>
2020-05-04 13:18:44 +0200

    fix eslint errors

--- js/script.js
+++ js/script.js
@@ -4,83 +4,84 @@ let upPressed;
 let initX = 0;
 let initY = 400;
 let groundLevel;
-let marioBack = document.getElementById('mario').offsetLeft;
-let marioFront = marioBack + 88;
-let pipeOffset = 400;
+const marioBack = document.getElementById('mario').offsetLeft;
+const marioFront = marioBack + 88;
+const pipeOffset = 400;
+const ground = 400;
+const step = 10;
+const jump = 10;
+const jumpHold = 100;
 
 function calcPipeBack() {
-  let gameFieldX = Math.abs(document.getElementById('gameField').offsetLeft); 
-  let pipeBack = (pipeOffset - (gameFieldX % pipeOffset));
+  const gameFieldX = Math.abs(document.getElementById('gameField').offsetLeft);
+  const pipeBack = (pipeOffset - (gameFieldX % pipeOffset));
   return pipeBack;
 }
 
 function createPipes(quantity) {
-  let pipe1 = document.createElement('div');
-  pipe1.setAttribute('id', 'pipe');
+  const pipe1 = document.createElement('div');
+  pipe1.setAttribute('class', 'pipe');
+  pipe1.style.left = `${pipeOffset}px`;
   document.getElementById('gameField').appendChild(pipe1);
-  let pipe = document.getElementById('pipe');
-  for (let i = 0; i < quantity; i++) {
-    let pipeClone = pipe.cloneNode(true);
+  const pipe = document.getElementsByClassName('pipe')[0];
+  for (let i = 2; i < quantity; i++) {
+    const pipeClone = pipe.cloneNode(true);
+    pipeClone.setAttribute('id', `pipe_${i}`);
     pipeClone.style.left = `${i * pipeOffset}px`;
     document.getElementById('gameField').appendChild(pipeClone);
   }
 }
 createPipes(10);
 
-function eventListener() {
-  document.addEventListener('keydown', keyDownHandler, false);
-  document.addEventListener('keyup', keyUpHandler, false);
-}
-
 function marioDown() {
   const time = setInterval(() => {
-    initY += 10;
+    initY += jump;
     document.getElementById('mario').style.top = `${initY}px`;
-    if (initY >= 400) {
+    if (initY >= ground) {
       clearInterval(time);
     }
   }, 50);
 }
 
 function onGround() {
-  if (document.getElementById('mario').offsetTop === 400) {
+  if (document.getElementById('mario').offsetTop === ground) {
     groundLevel = true;
     return groundLevel;
   }
+  return false;
 }
 
 function marioBeforePipe() {
   if (marioFront + 2 === calcPipeBack()) {
     rightPressed = false;
-  } 
-
+  }
 }
 
 function keyDownHandler(event) {
-  if (event.keyCode === 39) {
+  if (event.key === 'ArrowRight') {
     rightPressed = true;
     marioBeforePipe();
-    if (rightPressed) {    
-    initX -= 10;
-    document.getElementById('gameField').style.left = `${initX}px`;       
-     }  
+    if (rightPressed) {
+      initX -= step;
+      document.getElementById('gameField').style.left = `${initX}px`;
+    }
   }
-  if (event.keyCode === 37 && initX < 330) {
+  if (event.key === 'ArrowLeft' && initX < 330) {
     leftPressed = true;
-    initX += 10;
+    initX += step;
     document.getElementById('gameField').style.left = `${initX}px`;
   }
-  if (event.keyCode === 38 && initY > 200) {
-    let isOnGround = onGround();
+  if (event.key === 'ArrowUp' && initY > 200) {
+    const isOnGround = onGround();
     if (isOnGround) {
       upPressed = true;
-      initY -= 100;
+      initY -= jumpHold;
       document.getElementById('mario').style.top = `${initY}px`;
       marioDown();
     }
     if (upPressed) {
       setTimeout(() => {
-        initY -= 100;
+        initY -= jumpHold;
         document.getElementById('mario').style.top = `${initY}px`;
       }, 100);
     }
@@ -88,15 +89,19 @@ function keyDownHandler(event) {
 }
 
 function keyUpHandler(event) {
-  if (event.keyCode === 39) {
+  if (event.key === 'ArrowRight') {
     rightPressed = false;
   }
-  if (event.keyCode === 37) {
+  if (event.key === 'ArrowLeft') {
     leftPressed = false;
   }
-  if (event.keyCode === 38) {
+  if (event.key === 'ArrowUp') {
     upPressed = false;
   }
 }
 
+function eventListener() {
+  document.addEventListener('keydown', keyDownHandler, false);
+  document.addEventListener('keyup', keyUpHandler, false);
+}
 eventListener();
